a916096660478043a221313896cbe9b85fe908da
NoferOff <denis.horpynych99@gmail.com>
2025-12-15 15:22:51 +0200

    test commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Nice to meet you!I")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token("").build()
+    
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Бот запущено...")
+    app.run_polling()
