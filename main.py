06f049ddb539a9e7f707c7470cc668336f0aec6f
NoferOff <denis.horpynych99@gmail.com>
2025-12-15 15:24:08 +0200

    test commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Nice to meet you!I")
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token("").build()
+    app = ApplicationBuilder().token("0").build()
     
     app.add_handler(CommandHandler("start", start))
     
