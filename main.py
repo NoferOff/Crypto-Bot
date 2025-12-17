a6156719be69dc00b69f5633d78df1a405222042
NoferOff <denis.horpynych99@gmail.com>
2025-12-17 21:26:20 +0200

    I add some new text

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+import os
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler,MessageHandler
+
+load_dotenv()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Nice to meet you!I")
+    await update.message.reply_text("Hello! 👋\n"
+        "I’m your Crypto-Bot 🤖\n"
+        "I can show cryptocurrency prices, help check your balance, and perform actions.\n\n"
+        "Click a button below to choose an action ⬇️")
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token("0").build()
+    app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
     
     app.add_handler(CommandHandler("start", start))
     
     print("Бот запущено...")
-    app.run_polling()
+
+
+
+
+    KeyboardInterrupt
