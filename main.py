d2d8d1597005847d1a123ab2f62638d8ee7cf33e
NoferOff <denis.horpynych99@gmail.com>
2025-12-19 19:58:37 +0200

    I added new code

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# бібліотека
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder,CallbackQueryHandler,CommandHandler
 from dotenv import load_dotenv
 import os
-from telegram import InlineKeyboardButton,InlineKeyboardMarkup
-from telegram.ext import CallbackQueryHandler,MessageHandler
 
 load_dotenv()
+# токен
+ApplicationBuilder().token(os.getenv("BOT_TOKEN"))
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! 👋\n"
+
+
+# повідолення
+markup = InlineKeyboardMarkup([
+    [InlineKeyboardButton("📈 Show Prices", callback_data='show_prices')],
+    [InlineKeyboardButton("💰 Check Balance", callback_data='check_balance')],
+    [InlineKeyboardButton("⚙️ Settings", callback_data='settings')]
+])
+async def start(update, context):
+   await update.message.reply_text("Hello! 👋\n"
         "I’m your Crypto-Bot 🤖\n"
         "I can show cryptocurrency prices, help check your balance, and perform actions.\n\n"
-        "Click a button below to choose an action ⬇️")
+        "Click a button below to choose an action ⬇️", reply_markup=markup)
+   
 
+
+# кнопки
+async def buttons(update, context):
+    query = update.callback_query
+    await query.answer()
+
+# запуск бота
 if __name__ == "__main__":
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
-    
+
     app.add_handler(CommandHandler("start", start))
-    
+    app.add_handler(CallbackQueryHandler(buttons))
+
     print("Бот запущено...")
-
-
-
-
-    KeyboardInterrupt
+    app.run_polling()
